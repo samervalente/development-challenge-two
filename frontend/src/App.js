12ec34090afd7377799cca8470a6262582abb8cb
samervalente <samervalente@gmail.com>
2022-12-08 09:58:28 -0300

    chore: create frontend initial config

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import "./styles/reset.css";
+import "./styles/global.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Initial config</h1>
+      <button>Test</button>
+    </div>
+  );
+}
+
+export default App;
